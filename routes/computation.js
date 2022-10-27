82f644b80d525c67bb02819123624eb7bac0b857
sdraheem4 <112121575+sdraheem4@users.noreply.github.com>
2022-10-26 21:03:44 -0500

    created bonus

---
+++ routes/computation.js
@@ -0,0 +1,29 @@
+var express = require('express');
+
+var router = express.Router();
+
+var x = Math.random();
+
+var y = Math.random();
+
+
+
+var val1="Math.log(x) applied to "+ x + " is "+ Math.log(x)
+
+var val2="Math.imul(x, y) applied to "+ x +"and"+ y +" is "+ Math.imul(x, y)
+
+var val3= "Math.log10(x) applied to "+ x + " is "+ Math.log10(x);
+
+
+
+/* GET home page. */
+
+router.get('/', function(req, res, next) {
+
+  res.render('computation', { title:'ABDUL RAHEEM SYED   ',value1: val1, value2:vasl2, value3:val3 });
+
+});
+
+
+
+module.exports = router;
