8f6eccb64366cb8628d4090cf2fe4bc39f094f3d
sdraheem4 <112121575+sdraheem4@users.noreply.github.com>
2022-10-26 23:03:36 -0500

    updated computation.js

--- routes/computation.js
+++ routes/computation.js
@@ -9,7 +9,7 @@ var val2="Math.imul(x, y) applied to "+ x +"and"+ y +" is "+ Math.imul(x, y)
 var val3= "Math.log10(x) applied to "+ x + " is "+ Math.log10(x);
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('computation', { title:'ABDUL RAHEEM SYED   ',value1: val1, value2:val2, value3:val3 });
+  res.render('computation', { title:'ABDUL RAHEEM SYED   ',value1: val1, value2: val2, value3: val3 });
 });
 
 module.exports = router;
